83c10da32a918fed436a8d5ae3c4b3b03d1a2a3f
anish-kulkarni <anish245@gmail.com>
2016-07-16 23:24:59 +0530

    brick graphics included

--- brick_breaker.cpp
+++ brick_breaker.cpp
@@ -8,27 +8,33 @@ using namespace std;
     double H=400,L=200, //height ,length of window
            x,y,vx,vy, //position and velocity components of ball
            d,dp,s,h,delta, //present position(distance from left wall),previous position, size of bat, height of bat from ground, change in d due to one keypress
-           l,t, //dimensions of bricks length and thickness
-           dt;  //small time interval
-
+           l,t,gap, //dimensions of bricks length and thickness. gap between display of bricks.
+           dt,  //small time interval
+           delay_time=5; //delay between showing successive frames
 
 int main()
 {
     // initializations:
-    m=1;n=5;N=m*n;
-    x=L/2;y=H/20;vx=5;vy=20;
-    d=L/2;s=L/4;h=H/20; delta= s/20;
-    l=L/n;t=H/(3*m);
-    dt=0.02;
+    m=10;n=5;N=m*n;
+    x=L/2;y=H/20;vx=10;vy=20;
+    d=L/2;s=L/2;h=H/20; delta= s/20;
+    l=L/n;t=H/(3*m); gap = 1;
+    dt=0.1;
     //create bricks
     bool Brix[m][n];
     for(int i=0;i<m;i++)
         for(int j=0;j<n;j++)
-            Brix[i][j]=1;
+            Brix[i][j]=true;
     //display
-    int d=DETECT,m;
-    initgraph(&d,&m,"C:\\TC\\BGI");
+    int d=DETECT,z;
+    initgraph(&d,&z,"C:\\TC\\BGI");
     rectangle(0,0,L,H);
+    for(int i=0;i<m;i++)
+        for(int j=0;j<n;j++){
+            double xc = 0.5*l + j*l,
+                   yc = H - (0.5*t + i*t);
+            rectangle(xc - l/2 + gap, yc + t/2 - gap, xc + l/2 - gap, yc - t/2 + gap );
+        }
     //game starts
     while(y>=h-abs(vy)*dt)
     {
@@ -58,13 +64,13 @@ int main()
         circle(x,y,3);
         line(d-s/2,h,d+s/2,h);
         rectangle(0,0,L,H);
+        //check collision with brick
         int X,Y;    //stores coordinates of brick on which the ball is. The brick will then be broken if not yet broken.
         X = (x/l);
         Y = (H-y)/t;
         if(Y<0) Y=0;
         if(X<0) X=0;
         if(X>n-1) X=n-1;
-        //check collision with brick
         if(Y<m)
         {
             if(Brix[Y][X])
@@ -83,10 +89,14 @@ int main()
                 {
                     vx=-vx;vy=-vy;
                 }
+                double xc = 0.5*l + X*l,
+                       yc = H - (0.5*t + Y*t);
+                setcolor(BLACK);
+                rectangle(xc - l/2 + gap, yc + t/2 - gap, xc + l/2 - gap, yc - t/2 + gap );
             }
         }
 
-        delay(4);
+        delay(delay_time);
         setcolor(BLACK);
         circle(x,y,3);
         //collision with walls
